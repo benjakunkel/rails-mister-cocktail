5131745b8c2c1fa72e085a18a8f1b16830b6022f
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-07-24 12:18:05 -0300

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  root to: "/cocktails"
+  end
   resources :cocktails, only: [:index,:show, :new, :create, :destroy] do 
     resources :doses, only: [:new, :create, :destroy]
   end
