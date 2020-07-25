7f12dda1536bd152b77dd4922639414aaaca959e
(Benja Kunkel) <kunkelbenjamin08@gmail.com>
2020-07-25 17:51:36 -0300

    arreglo de imagenes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,12 @@
 class ReviewsController < ApplicationController
-  
+
   def new
-    # we need @restaurant in our `simple_form_for`
     @cocktail = Cocktail.find(params[:cocktail_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    # we need `restaurant_id` to associate review with corresponding restaurant
     @cocktail = Cocktail.find(params[:cocktail_id])
     @review.cocktail = @cocktail
     @review.save
@@ -20,5 +18,5 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:content, :rating)
   end
- 
+
 end
